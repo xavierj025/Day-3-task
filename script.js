a7cf8d1ddb0863442bf79ee0337c7c26f1cc0f66
xavierj025 <gibinralph@gmail.com>
2022-12-04 16:06:16 +0530

    Day3 task

---
+++ script.js
@@ -0,0 +1,10 @@
+var resume = {
+    "name": "Xavier j",
+    "Age": "24",
+    "gender": "male",
+    "qualification": ["10th", "12th", "degeree"],
+    "location": "chennai",
+    "experience": "1 years of experinence in xxx company",
+    "LanguagesKnown": ["tamil", "english"],
+}
+console.log(resume)
